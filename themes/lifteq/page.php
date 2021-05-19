7261e999f69b6185c9f6f323ce4d03ae791cae0e
Rannojit <cserannojitsarkar22@gmail.com>
2021-05-19 17:49:20 +0600

    .

--- themes/lifteq/page.php
+++ themes/lifteq/page.php
@@ -2,10 +2,18 @@
 get_header();
 ?>
 
-<?php
 
-the_content();
- ?>
+<section class="page-content">
+  <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="page-cnt-inner">
+				<?php the_content();?>
+           </div>
+        </div>
+      </div>
+  </div>    
+</section>
 <?php 
 get_footer();
 ?>
