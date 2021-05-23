978792d68e044953869af55aa0abe79fe02cacb4
haque <azizul.softsw@gmail.com>
2021-05-23 18:22:14 +0600

    Update product-image.php

--- themes/lifteq/woocommerce/single-product/product-image.php
+++ themes/lifteq/woocommerce/single-product/product-image.php
@@ -43,23 +43,24 @@ $attachment_ids = $product->get_gallery_image_ids();
 		<?php
 		if ( $product->get_image_id() ) {
 			$imgurl = wp_get_attachment_image_url( $product->get_image_id(), 'full' );
+			$thumimgtag = wp_get_attachment_image( $product->get_image_id(), 'full' );
 			echo '<div class="woocommerce-product-gallery__image">';
             echo '<a class="woocommerce-main-image fancybox" data-fancybox="gallery" href="'.$imgurl.'">';
-            echo '<div class="wc-main-images" style="background:url('.$imgurl.');background-repeat:no-repeat;"></div>';
+            echo $thumimgtag;
             echo '</a>';
             echo '</div>';
 		} else {
 			echo '<div class="woocommerce-product-gallery__image--placeholder">';
-			echo '<div class="wc-main-images" style="background:url('.wc_placeholder_img_src( 'woocommerce_single' ).');background-repeat:no-repeat;"></div>';
+			echo sprintf( '<img src="%s" alt="%s" class="wp-post-image" />', esc_url( wc_placeholder_img_src( 'woocommerce_single' ) ), esc_html__( 'Awaiting product image', 'woocommerce' ) );
 			echo '</div>';
 		}
         if ( $attachment_ids && $product->get_image_id() ) {
 			foreach ( $attachment_ids as $attachment_id ) {
 				$thumimgurl = wp_get_attachment_image_url( $attachment_id, 'full' );
-				//$thumimgtag = wp_get_attachment_image_url( $attachment_id, 'full' );
+				$thumimg = wp_get_attachment_image( $attachment_id, 'full' );
 				echo '<div class="woocommerce-product-gallery__image">';
 	            echo '<a class="woocommerce-main-image fancybox" data-fancybox="gallery" href="'.$thumimgurl.'">';
-	            echo '<div class="wc-main-images" style="background:url('.$thumimgurl.');background-repeat:no-repeat;"></div>';
+	            echo $thumimg;
 	            echo '</a>';
 	            echo '</div>';
 			}
